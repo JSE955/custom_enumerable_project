86f84a2649b3dc93347c9bfabe003226cf4e82e2
JSE955 <jordanscottedwards@gmail.com>
2024-05-13 11:27:42 -0400

    Complete each with index

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,11 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    for i in (0...self.length)
+      yield(self[i], i)
+    end
+    return self
+  end
 end
 
 # You will first have to define my_each
